dbda2aecba902521fcbcf305061fb6563216097c
landonbuell <lhb1007@wildcats.unh.edu>
2021-07-29 22:17:09 -0400

    Added Framework Solution and Source Project with Main.cpp

---
+++ Framework/Source/Main.cpp
@@ -0,0 +1,23 @@
+/*
+Author:		Landon Buell
+Date:		July 2021
+Repo:		Deep-Learning-Application
+Solution:	DeepLearningFramework
+Project:	DeepLearningBackend
+File:		Source.cpp
+Description:
+	TODO: Write Description
+*/
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+int main(int argc, char** argv)
+{
+	// Main Executable 
+	int EXIT_STATUS = 0;
+
+
+	return EXIT_STATUS;
+}

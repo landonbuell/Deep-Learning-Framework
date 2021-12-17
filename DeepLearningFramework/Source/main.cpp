b8d39610b47fcab646c6d2c17aea6c3a60413046
LandonBuell <42503615+landonbuell@users.noreply.github.com>
2021-12-17 17:56:06 -0500

    Completed Indexer for Arbitrary Rank Tensor

--- DeepLearningFramework/Source/main.cpp
+++ DeepLearningFramework/Source/main.cpp
@@ -16,23 +16,28 @@
 #include <vector>
 
 #include "Tensor.h"
-#include "Tensor1D.h"
-#include "Tensor2D.h"
-#include "Tensor3D.h"
-#include "Tensor4D.h"
 
 int main(int argc, char** argv)
 {
 	int EXIT_STATUS = 0;
 
+	// Create a Local Array
 	const int size = 16;
 	float* arr = new float[size];
 	for (int i = 0; i < size; i++)
-		arr[i] = i;
+		arr[i] = (float)i;
 	
 	// Create Initial Tensor
+	Tensor alpha(arr, size);
+	alpha[Indexer{ 0 }] = 4.0f;
+	alpha.describe(std::cout);
+
+	alpha.reshape(TensorShape{ 2,2,4 });
+	alpha[Indexer{ 1,0,1 }] = 70.0f;
+	alpha.describe(std::cout);
 
 
+	// Free Memory (handled by shared ptr)
 	std::cout << "=)" << std::endl;
 
 }
